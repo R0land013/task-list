8421136122bc34885d6580a1d003f6703cdbae83
Rolando Rio <rolandorio06@gmail.com>
2023-06-25 14:00:32 -0400

    Started project

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    plugins: ['react-refresh', 'react'],
+    rules: {
+        'react-refresh/only-export-components': 'warn',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'semi': 'off',
+        '@typescript-eslint/semi': 'error'
+    },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+    return (
+        <h1 className="text-3xl font-bold underline">Task List</h1>
+    )
+}
+
+export default App
